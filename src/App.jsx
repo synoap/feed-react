09b6b2e661409cc0ea152172675afedd7eab1d7a
synoap <devsynoap@gmail.com>
2024-03-23 14:12:52 -0300

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 
+import styles from "./App.module.css";
+
 import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post author="John Doe" content="This is my first post" />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="John Doe" content="This is my first post" />
+          <Post author="Oprah" content="This is my second post" />
+        </main>
+      </div>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import styles from "./Header.module.css";
 
+import vite from "../assets/vite.svg";
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>Feed</strong>
+      <p>Feed</p>
+      <img src={vite} alt="logo" />
     </header>
   );
 }
